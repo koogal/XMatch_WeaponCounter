b6fa7349bf360bf7e4c5a40d0a777314e3579158
Tackky <roundshiny3ki.t@gmail.com>
2025-08-24 18:59:28 +0900

    update search and add saving data as csv

---
+++ WeaponCounter.py
@@ -0,0 +1,30 @@
+import cv2 as cv
+import numpy as np
+import glob
+from matplotlib import pyplot as plt
+
+WeaponLists = glob.glob("WeaponList/*")
+resultlist = np.array([])
+
+img_rgb = cv.imread('testimage.png',cv.IMREAD_COLOR_BGR)
+assert img_rgb is not None, "file could not be read, check with os.path.exists()"
+img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
+
+for weapon_dir in WeaponLists:    
+    template = cv.imread(weapon_dir, cv.IMREAD_GRAYSCALE) 
+    assert template is not None, "file could not be read, check with os.path.exists()"
+
+    result = cv.matchTemplate(img_gray, template, cv.TM_CCORR_NORMED)
+    th, tw = template.shape[:2]
+    threshold = 0.95
+    loc = np.where(result >= threshold)
+    w, h = template.shape
+
+    weapon_filename = weapon_dir.replace("WeaponList\\", "")
+    weapon_name = weapon_filename.replace(".png", "")
+    print(weapon_name,len(loc[0]))
+    resultlist = np.append(resultlist, [weapon_name, len(loc[0])])
+
+resultlist = resultlist.reshape(-1, 2)
+print(resultlist)
+np.savetxt("result.csv",resultlist, fmt='%s')
--- multi_detect_test.py
+++ multi_detect_test.py
@@ -1,18 +1,17 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
-
-img_rgb = cv.imread('testimage.png')
+ 
+img_rgb = cv.imread('testimage.png', cv.IMREAD_ANYDEPTH)
 assert img_rgb is not None, "file could not be read, check with os.path.exists()"
-img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
-template = cv.imread('WeaponList/Incbrush_Nouveau.png', cv.IMREAD_GRAYSCALE)
+template = cv.imread('WeaponList/Incbrush_Nouveau.png', cv.IMREAD_ANYDEPTH)
 assert template is not None, "file could not be read, check with os.path.exists()"
 w, h = template.shape[::-1]
-
-res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
+ 
+res = cv.matchTemplate(img_rgb,template,cv.TM_CCOEFF_NORMED)
 threshold = 0.8
 loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
-    cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-
-cv.imwrite('res.png',img_rgb)
+    cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 1)
+ 
+cv.imwrite('mul_res.png',img_rgb)
